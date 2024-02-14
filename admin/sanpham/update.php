2c968b276693b8b9eb169d1c6136cfec1b7bd233
nguyenthiminh04 <nguyenthiminhcute07@gmail.com>
2024-02-14 08:58:22 +0700

    update

--- admin/sanpham/update.php
+++ admin/sanpham/update.php
@@ -12,7 +12,30 @@ if (is_array($suasanpham)) {
             <input type="hidden" name="id" value="<?= $id ?>">
             <div class="row2 mb10">
             <label style="color:#000; text-transform: uppercase; margin-bottom: 20px;">Danh muc sản phẩm</label> <br>
-            </div>
+            <select class="form-select" name="iddm" aria-label="Default select example" id="">
+                <?php
+                foreach ($select_danhmuc as $optiondanhmuc ) {
+                    extract($optiondanhmuc);
+                    if ($iddm == $id) {
+                        ?>
+                        <option value="<?= $id ?>" selectd><?= $name?></option>
+                        <?php
+                    }else{
+                        ?>
+                        <option value="<?= $id ?>"><?= $name ?></option>
+                        <?php
+                    }
+                }
+                ?>
+            </select>
+        </div>
+
+        <div class="row2 mb10 form_content_container">
+        <label for="" style="color:#000; text-transform: uppercase; margin-bottom: 20px;">Tên sản phẩm</label> <br>
+        <input type="text" style="height: 40px;" value="<?= $suasanpham['name'] ?>" name="tensanpham" required placeholder="Nhập vào tên sản phẩm">
+        
+    </div>
+        
         </form>
     </div>
 </div>
